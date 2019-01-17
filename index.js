bc8f226a3d27f0cfe8ae433ebedcdb09da47221a
matth3wn <matth3wn@gmail.com>
2019-01-17 12:58:06 -0800

    finished most of the program

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function getUserInput(){
 // update the DOM with the new element containing the container div filled with user input
 
 function addHtmlGenerator(input){
-	$('.shopping-list').append(`
+  $('.shopping-list').append(`
 	<li>
         <span class="shopping-item">${input}</span>
         <div class="shopping-item-controls">
@@ -26,7 +26,28 @@ function addHtmlGenerator(input){
 }
 
 // when user clicks check, update style of value in target div to be strikethrough
-
+function crossText(){
+  //$('.shopping-class').find('li')
+  $('ul').on('click','.shopping-item-toggle', function(event){
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked'); 
+  });
+}
 // when user clicks delete, remove the target's container div
+function deleteListItem(){
+  $('ul').on('click', '.shopping-item-delete', function(event){
+    $(this).closest('li').remove();
+  });
+}
 
-$(getUserInput);
+// on main function use .on 
+function main(){
+  $('#js-shopping-list-form').submit(function(event){
+    event.preventDefault();
+    const userPut = getUserInput();
+    addHtmlGenerator(userPut);
+  });
+  crossText();
+  deleteListItem();
+}
+
+$(main);
